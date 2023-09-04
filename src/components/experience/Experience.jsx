48d325d515bfa8f5f7d9cdf2cfa539f1dad624d8
Chrispy <christiancarteno@hotmail.com>
2023-09-03 23:51:50 -0700

    added php to experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -61,6 +61,13 @@ const Experience = () => {
         <div className='experience__backend'>
         <h3>Backend Development</h3>
           <div className='experience__content'>
+          <article className='experience__details'>
+              <BsPatchCheck className='experience__details-icon' />
+              <div>
+              <h4>PHP</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
             <article className='experience__details'>
               <BsPatchCheck className='experience__details-icon' />
               <div>
